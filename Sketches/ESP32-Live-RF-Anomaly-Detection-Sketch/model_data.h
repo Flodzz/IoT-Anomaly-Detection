e9a1f51dc6a142bc1f2e64e75dca7902661aff72
Hayden Moseley <hwlodarz@hotmail.com>
2026-04-16 20:06:30 +0100

    Create a dir for sketches to go in

---
+++ Sketches/ESP32-Live-RF-Anomaly-Detection-Sketch/model_data.h
@@ -0,0 +1,8 @@
+#ifndef TEST_DATA_H
+#define TEST_DATA_H
+
+// Scaling parameters (RobustScaler: (x - median) / IQR)
+double medians[] = {8081.0, 40.0, 0.0, 36182, 0.00000596};
+double iqrs[] = {37192.0, 1.0, 13333333.33333333, 28282.0, 0.00024295};
+
+#endif
